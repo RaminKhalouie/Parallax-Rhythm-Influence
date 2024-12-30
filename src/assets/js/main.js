d1aa4657a18c33bec56f6b88045f229bab274bd2
Ramin Khalouie <ra.khalouie@gmail.com>
2024-12-30 14:20:12 +0330

    Complete influnce section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,9 @@ const nav = document.getElementById('nav-menu');
 const lis = nav.querySelectorAll('li>a>span');
 const main = document.querySelector('main');
 
+const masks = document.querySelectorAll('.mask');
+const imagesMask = document.querySelectorAll('.mask-image');
+
 toggle.addEventListener('click', () => {
     nav.classList.remove('translate-x-full');
     lis.forEach((el) => {
@@ -18,8 +21,27 @@ close.addEventListener('click', () => {
 });
 
 
+// masks.forEach((el) => {
+//     el.dataset.top = el.offsetTop;
+// });
+
 window.addEventListener('scroll', () => {
     const sy = window.scrollY;
     main.style.transform = 'translateY(-' + (sy / 3) + 'px)';
 
+    masks.forEach((el) => {
+        if ((sy - (window.innerHeight / 2) >= el.offsetTop)) {
+            el.classList.add('active');
+        } else {
+            el.classList.remove('active');
+        }
+    });
+
+    imagesMask.forEach((el) => {
+        if ((sy - (window.innerHeight / 2) >= el.offsetTop)) {
+            el.classList.add('w-40');
+        } else {
+            el.classList.remove('w-40');
+        }
+    });
 });
